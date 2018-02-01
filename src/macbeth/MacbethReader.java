577799c34380f44bf427721504d6d29a46bdd9fe
mj-gibson1701 <mj.gibson1701@gmail.com>
2018-02-01 10:39:32 -0500

    Update MacbethReader.java

--- src/macbeth/MacbethReader.java
+++ src/macbeth/MacbethReader.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package macbeth;
 import java.util.*;
 import java.io.*;
@@ -51,4 +46,3 @@ public class MacbethReader {
     }
 }
     
-
